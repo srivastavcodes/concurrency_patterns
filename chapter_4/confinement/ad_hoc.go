8a20a8696c5fb9d6ec67a9219269ac94fea2fe21
Parth Srivastav <parthsrivastav.00@gmail.com>
2025-09-14 11:36:58 +0530

    examples for lexical confinement

--- chapter_4/confinement/ad_hoc.go
+++ chapter_4/confinement/ad_hoc.go
@@ -1,54 +1,11 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
-	"sync"
 )
 
 func main() {
 	lexicalConfinementBuffer()
-}
-
-func lexicalConfinementBuffer() {
-	printData := func(wg *sync.WaitGroup, data []byte) {
-		defer wg.Done()
-
-		var buf bytes.Buffer
-		for _, b := range data {
-			fmt.Fprintf(&buf, "%c", b)
-		}
-		fmt.Println(buf.String())
-	}
-	var wg sync.WaitGroup
-	wg.Add(2)
-	data := []byte("stickynotes")
-	go printData(&wg, data[:6])
-	go printData(&wg, data[6:])
-	wg.Wait()
-	// data remains as it, copy is passed around
-	fmt.Println(string(data))
-}
-
-func lexicalConfinement() {
-	chanOwner := func() <-chan int {
-		results := make(chan int)
-		go func() {
-			defer close(results)
-			for i := 0; i < 5; i++ {
-				results <- i
-			}
-		}()
-		return results
-	}
-	consumer := func(results <-chan int) {
-		for data := range results {
-			fmt.Printf("received: %d\n", data)
-		}
-		fmt.Println("done receiving")
-	}
-	results := chanOwner()
-	consumer(results)
 }
 
 func adhocConfinement() {
---
+++ chapter_4/confinement/lexical.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"sync"
+)
+
+func lexicalConfinementBuffer() {
+	printData := func(wg *sync.WaitGroup, data []byte) {
+		defer wg.Done()
+
+		var buf bytes.Buffer
+		for _, b := range data {
+			fmt.Fprintf(&buf, "%c", b)
+		}
+		fmt.Println(buf.String())
+	}
+	var wg sync.WaitGroup
+	wg.Add(2)
+	data := []byte("stickynotes")
+	go printData(&wg, data[:6])
+	go printData(&wg, data[6:])
+	wg.Wait()
+	// data remains as it, copy is passed around
+	fmt.Println(string(data))
+}
+
+func lexicalConfinement() {
+	chanOwner := func() <-chan int {
+		results := make(chan int)
+		go func() {
+			defer close(results)
+			for i := 0; i < 5; i++ {
+				results <- i
+			}
+		}()
+		return results
+	}
+	consumer := func(results <-chan int) {
+		for data := range results {
+			fmt.Printf("received: %d\n", data)
+		}
+		fmt.Println("done receiving")
+	}
+	results := chanOwner()
+	consumer(results)
+}
